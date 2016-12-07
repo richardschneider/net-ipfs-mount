eda1296c1f153c76c8cc60402900759fbcfe240b
Richard Schneider <makaretu@gmail.com>
2016-12-07 18:32:20 +1300

    refactor(dokan): group the operations

--- IpfsMount/IpfsDokan.cs
+++ IpfsMount/IpfsDokan.cs
@@ -21,19 +21,9 @@ namespace IpfsMount
         static string[] rootFolders = { "ipfs", "ipns" };
         static IpfsClient ipfs = new IpfsClient();
 
-        public void Cleanup(string fileName, DokanFileInfo info)
-        {
-            // Nothing to do.
-        }
-
-        public void CloseFile(string fileName, DokanFileInfo info)
-        {
-            // Nothing to do.
-        }
-
         public NtStatus CreateFile(string fileName, DokanNet.FileAccess access, FileShare share, FileMode mode, FileOptions options, FileAttributes attributes, DokanFileInfo info)
         {
-            Console.WriteLine("CreateFile NYI, {0}, mode {1}", fileName, mode);
+            Console.WriteLine("CreateFile NYI, {0}, mode {1}, access {2}", fileName, mode, access);
 
             // Read only access.
             if (mode != FileMode.Open)
@@ -85,92 +75,6 @@ namespace IpfsMount
             return DokanResult.Success;
         }
 
-        public NtStatus DeleteDirectory(string fileName, DokanFileInfo info)
-        {
-            Console.WriteLine("DeleteDirectory NYI");
-            throw new NotImplementedException();
-        }
-
-        public NtStatus DeleteFile(string fileName, DokanFileInfo info)
-        {
-            Console.WriteLine("DeleteFile NYI");
-            throw new NotImplementedException();
-        }
-
-        public NtStatus FindFiles(string fileName, out IList<FileInformation> files, DokanFileInfo info)
-        {
-            Console.WriteLine("FindFiles, {0}", fileName);
-
-            var file = info.Context as IpfsFile;
-            if (file != null)
-            {
-                files = file.Links
-                    .Select(link => new FileInformation()
-                    {
-                        FileName = link.Name,
-                        Length = link.Size,
-                        Attributes = link.IsDirectory 
-                            ? FileAttributes.Directory 
-                            : FileAttributes.Normal
-                    })
-                    .ToList();
-                return DokanResult.Success;
-            }
-
-            // The root consists only of the root folders.
-            if (fileName == rootName)
-            {
-                files = rootFolders
-                    .Select(name => new FileInformation()
-                    {
-                        FileName = name,
-                        Attributes = FileAttributes.Directory,
-                        LastAccessTime = DateTime.Now
-                    })
-                    .ToList();
-                return DokanResult.Success;
-            }
-
-            // Can not determine the contents of the root folders.
-            if (rootFolders.Any(name => fileName == (rootName + name)))
-            {
-                files = new FileInformation[0];
-                return DokanResult.Success;
-            }
-
-            files = new FileInformation[0];
-            return DokanResult.NotImplemented;
-        }
-
-        public NtStatus FindFilesWithPattern(string fileName, string searchPattern, out IList<FileInformation> files, DokanFileInfo info)
-        {
-            Console.WriteLine("FindFilesWithPattern NYI {0} {1}", fileName, searchPattern);
-            files = new FileInformation[0];
-            return DokanResult.NotImplemented;
-        }
-
-        public NtStatus FindStreams(string fileName, out IList<FileInformation> streams, DokanFileInfo info)
-        {
-            Console.WriteLine("FindStreams NYI");
-            throw new NotImplementedException();
-        }
-
-        public NtStatus FlushFileBuffers(string fileName, DokanFileInfo info)
-        {
-            Console.WriteLine("FlushFileBuffers NYI");
-            throw new NotImplementedException();
-        }
-
-        public NtStatus GetDiskFreeSpace(out long freeBytesAvailable, out long totalNumberOfBytes, out long totalNumberOfFreeBytes, DokanFileInfo info)
-        {
-            Console.WriteLine("GetFreeSpace");
-            freeBytesAvailable = 0;
-            totalNumberOfBytes = 0;
-            totalNumberOfFreeBytes = 0;
-
-            return NtStatus.Success;
-        }
-
         public NtStatus GetFileInformation(string fileName, out FileInformation fileInfo, DokanFileInfo info)
         {
             Console.WriteLine("GetFileInformation {0}", fileName);
@@ -213,10 +117,21 @@ namespace IpfsMount
             throw new NotImplementedException();
         }
 
+        #region Volumne Operations
+        public NtStatus GetDiskFreeSpace(out long freeBytesAvailable, out long totalNumberOfBytes, out long totalNumberOfFreeBytes, DokanFileInfo info)
+        {
+            Console.WriteLine("GetFreeSpace");
+            freeBytesAvailable = 0;
+            totalNumberOfBytes = 0;
+            totalNumberOfFreeBytes = 0;
+
+            return NtStatus.Success;
+        }
+
         public NtStatus GetVolumeInformation(
-            out string volumeLabel, 
-            out FileSystemFeatures features, 
-            out string fileSystemName, 
+            out string volumeLabel,
+            out FileSystemFeatures features,
+            out string fileSystemName,
             DokanFileInfo info)
         {
             Console.WriteLine("Get volumne info");
@@ -228,24 +143,21 @@ namespace IpfsMount
             return NtStatus.Success;
         }
 
-        public NtStatus LockFile(string fileName, long offset, long length, DokanFileInfo info)
-        {
-            Console.WriteLine("LockFile NYI");
-            return NtStatus.NotImplemented;
-        }
-
         public NtStatus Mounted(DokanFileInfo info)
         {
             Console.WriteLine("Mounted");
             return NtStatus.Success;
         }
 
-        public NtStatus MoveFile(string oldName, string newName, bool replace, DokanFileInfo info)
+        public NtStatus Unmounted(DokanFileInfo info)
         {
-            Console.WriteLine("MoveFile NYI");
-            throw new NotImplementedException();
+            Console.WriteLine("Unmounted NYI");
+            return DokanResult.Success;
         }
 
+        #endregion
+
+        #region File Operations
         public NtStatus ReadFile(string fileName, byte[] buffer, out int bytesRead, long offset, DokanFileInfo info)
         {
             Console.WriteLine("ReadFile NYI, buffer size {0}", buffer.Length);
@@ -269,48 +181,142 @@ namespace IpfsMount
             throw new NotImplementedException();
         }
 
-        public NtStatus SetAllocationSize(string fileName, long length, DokanFileInfo info)
+        public NtStatus LockFile(string fileName, long offset, long length, DokanFileInfo info)
         {
-            Console.WriteLine("SetAllocationSize NYI");
-            throw new NotImplementedException();
-        }
-
-        public NtStatus SetEndOfFile(string fileName, long length, DokanFileInfo info)
-        {
-            throw new NotImplementedException();
-        }
-
-        public NtStatus SetFileAttributes(string fileName, FileAttributes attributes, DokanFileInfo info)
-        {
-            throw new NotImplementedException();
-        }
-
-        public NtStatus SetFileSecurity(string fileName, FileSystemSecurity security, AccessControlSections sections, DokanFileInfo info)
-        {
-            throw new NotImplementedException();
-        }
-
-        public NtStatus SetFileTime(string fileName, DateTime? creationTime, DateTime? lastAccessTime, DateTime? lastWriteTime, DokanFileInfo info)
-        {
-            throw new NotImplementedException();
+            return DokanResult.Success; // LockFile does nothing
         }
 
         public NtStatus UnlockFile(string fileName, long offset, long length, DokanFileInfo info)
         {
-            throw new NotImplementedException();
-        }
-
-        public NtStatus Unmounted(DokanFileInfo info)
-        {
-            Console.WriteLine("Unmounted NYI");
-            throw new NotImplementedException();
+            return DokanResult.Success; // UnlockFile does nothing
         }
 
         public NtStatus WriteFile(string fileName, byte[] buffer, out int bytesWritten, long offset, DokanFileInfo info)
         {
-            Console.WriteLine("WriteFile NYI");
-            throw new NotImplementedException();
+            bytesWritten = 0;
+            return DokanResult.AccessDenied;
         }
+
+        public NtStatus DeleteFile(string fileName, DokanFileInfo info)
+        {
+            return DokanResult.AccessDenied;
+        }
+
+        public NtStatus MoveFile(string oldName, string newName, bool replace, DokanFileInfo info)
+        {
+            return DokanResult.AccessDenied;
+        }
+
+        public NtStatus SetAllocationSize(string fileName, long length, DokanFileInfo info)
+        {
+            return DokanResult.AccessDenied;
+        }
+
+        public NtStatus SetEndOfFile(string fileName, long length, DokanFileInfo info)
+        {
+            return DokanResult.AccessDenied;
+        }
+
+        public NtStatus SetFileAttributes(string fileName, FileAttributes attributes, DokanFileInfo info)
+        {
+            return DokanResult.AccessDenied;
+        }
+
+        public NtStatus SetFileSecurity(string fileName, FileSystemSecurity security, AccessControlSections sections, DokanFileInfo info)
+        {
+            return DokanResult.AccessDenied;
+        }
+
+        public NtStatus SetFileTime(string fileName, DateTime? creationTime, DateTime? lastAccessTime, DateTime? lastWriteTime, DokanFileInfo info)
+        {
+            return DokanResult.AccessDenied;
+        }
+        #endregion
+
+        #region Directory Operations
+        public NtStatus FindFiles(string fileName, out IList<FileInformation> files, DokanFileInfo info)
+        {
+            Console.WriteLine("FindFiles, {0}", fileName);
+
+            var file = info.Context as IpfsFile;
+            if (file != null)
+            {
+                files = file.Links
+                    .Select(link => new FileInformation()
+                    {
+                        FileName = link.Name,
+                        Length = link.Size,
+                        Attributes = link.IsDirectory
+                            ? FileAttributes.Directory
+                            : FileAttributes.Normal
+                    })
+                    .ToList();
+                return DokanResult.Success;
+            }
+
+            // The root consists only of the root folders.
+            if (fileName == rootName)
+            {
+                files = rootFolders
+                    .Select(name => new FileInformation()
+                    {
+                        FileName = name,
+                        Attributes = FileAttributes.Directory,
+                        LastAccessTime = DateTime.Now
+                    })
+                    .ToList();
+                return DokanResult.Success;
+            }
+
+            // Can not determine the contents of the root folders.
+            if (rootFolders.Any(name => fileName == (rootName + name)))
+            {
+                files = new FileInformation[0];
+                return DokanResult.Success;
+            }
+
+            files = new FileInformation[0];
+            return DokanResult.NotImplemented;
+        }
+
+        public NtStatus FindFilesWithPattern(string fileName, string searchPattern, out IList<FileInformation> files, DokanFileInfo info)
+        {
+            Console.WriteLine("FindFilesWithPattern NYI {0} {1}", fileName, searchPattern);
+            files = new FileInformation[0];
+            return DokanResult.NotImplemented;
+        }
+
+        public NtStatus DeleteDirectory(string fileName, DokanFileInfo info)
+        {
+            Console.WriteLine("DeleteDirectory NYI");
+            return DokanResult.AccessDenied;
+        }
+
+        #endregion
+
+        #region Misc Operations
+        public void Cleanup(string fileName, DokanFileInfo info)
+        {
+            // Nothing to do.
+        }
+
+        public void CloseFile(string fileName, DokanFileInfo info)
+        {
+            // Nothing to do.
+        }
+
+        public NtStatus FindStreams(string fileName, out IList<FileInformation> streams, DokanFileInfo info)
+        {
+            streams = null;
+            return DokanResult.NotImplemented;
+        }
+
+        public NtStatus FlushFileBuffers(string fileName, DokanFileInfo info)
+        {
+            return DokanResult.Success;
+        }
+        #endregion
+
     }
 }
 
