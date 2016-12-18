1c649654ea0faa8e949c03c80395cfd08223a6d2
Richard Schneider <makaretu@gmail.com>
2016-12-19 12:13:50 +1300

    test(directory): tests for /ipfs and /ipns

---
+++ IpfsMountTests/IpfsDirectoryTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ipfs.VirtualDisk;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ipfs.VirtualDisk.Tests
+{
+    [TestClass]
+    public class IpfsDirectoryTest
+    {
+
+        [TestMethod]
+        public void Exists()
+        {
+            Assert.IsTrue(Directory.Exists("t:/ipfs"));
+        }
+
+        [TestMethod]
+        public void Has_No_Files()
+        {
+            var info = new DirectoryInfo("t:/ipfs");
+
+            Assert.IsTrue(info.Exists);
+            Assert.AreEqual(0, info.GetFileSystemInfos("*.*").Length);
+        }
+
+    }
+}
---
+++ IpfsMountTests/IpnsDirectoryTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ipfs.VirtualDisk;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ipfs.VirtualDisk.Tests
+{
+    [TestClass]
+    public class IpnsDirectoryTest
+    {
+
+        [TestMethod]
+        public void Exists()
+        {
+            Assert.IsTrue(Directory.Exists("t:/ipns"));
+        }
+
+        [TestMethod]
+        public void Has_No_Files()
+        {
+            var info = new DirectoryInfo("t:/ipns");
+
+            Assert.IsTrue(info.Exists);
+            Assert.AreEqual(0, info.GetFileSystemInfos("*.*").Length);
+        }
+
+    }
+}
